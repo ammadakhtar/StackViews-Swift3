d7b950bcb636dc93aa7b4ccb17ba0ae856e46836
Ammad <ammadakhtar@live.com>
2017-03-28 23:44:48 +0500

    code upload

---
+++ stack view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stack view
+//
+//  Created by Ammad on 19/01/2017.
+//  Copyright © 2017 Ammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
